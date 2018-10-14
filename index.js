ebfa1a9c65eee1226758830de469edd19d1d5122
Samuel Ellis <pleasecastsamellis@gmail.com>
2018-10-14 17:06:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(name, array) {
   array.unshift(name);
-  return "Welcome $/name. You are number ($array.length) in line."
+  return "Welcome $/name. You are number $/array.length in line."
 }
