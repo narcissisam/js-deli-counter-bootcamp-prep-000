3ad1ee3e9ffb652b0423ff63ff85eb6f1f36b601
Samuel Ellis <pleasecastsamellis@gmail.com>
2018-10-14 17:39:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,5 @@ var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-  return ('Welcome, ${name}. You are number ${katzDeliLine.length} in line.');
-}
-function nowServing(katzDeliLine){
-  let i = 0;
-  while (katzDeliLine.length > i)
+  return ('Welcome,' +name+'. You are number' +katzDeliLine.length+' in line.');
 }
