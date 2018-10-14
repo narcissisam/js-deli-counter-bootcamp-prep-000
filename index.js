14fa659557e1612a0daf3db29be5bb74fe2e5c42
Samuel Ellis <pleasecastsamellis@gmail.com>
2018-10-14 17:41:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   return ('Welcome, ' +name+'. You are number ' +katzDeliLine.length+' in line.');
 }
+function nowServing(katzDeliLine) {
+  if katzDeliLine.length = 0{
+    
+  }
+}
